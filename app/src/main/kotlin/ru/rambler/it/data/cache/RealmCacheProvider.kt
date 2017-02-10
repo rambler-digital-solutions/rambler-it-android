b723f10415970935f8cada244444cedc5acab8ad
Tkachenko Artem <a.tkachenko@rambler-co.ru>
2017-02-10 12:47:55 +0300

    Refactor RealmCacheProvider

--- app/src/main/kotlin/ru/rambler/it/data/cache/RealmCacheProvider.kt
+++ app/src/main/kotlin/ru/rambler/it/data/cache/RealmCacheProvider.kt
@@ -5,39 +5,39 @@ import ru.rambler.it.data.dbo.EventDbo
 import ru.rambler.it.data.mappers.EventMapper
 import ru.rambler.it.domain.entities.Event
 import rx.Observable
-import java.util.*
 import javax.inject.Inject
 
-class RealmCacheProvider@Inject constructor(val eventMapper: EventMapper) : CacheProvider {
+class RealmCacheProvider @Inject constructor(val eventMapper: EventMapper) : CacheProvider {
+
 
     override fun saveEvents(data: List<Event>): Observable<List<Event>> {
-        val realm = Realm.getDefaultInstance()
-        try {
-            val dboList = ArrayList<EventDbo>()
-            data.mapTo(dboList) { eventMapper.mapToDbo(it) }
-            realm.beginTransaction()
-            realm.copyToRealmOrUpdate(dboList)
-            realm.commitTransaction()
-        } catch (e: Exception) {
-            println(e)
-        } finally {
-            realm.close()
-            return getEventsFromCache()
-        }
+        return Observable.from(data)
+                .map { eventMapper.mapToDbo(it) }
+                .toList()
+                .doOnNext {
+                    val realm = Realm.getDefaultInstance()
+                    realm.beginTransaction()
+                    realm.copyToRealmOrUpdate(it)
+                    realm.commitTransaction()
+                    realm.close()
+                }.flatMap { getEventsFromCache() }
     }
 
     override fun getEventsFromCache(): Observable<List<Event>> {
-        val result = ArrayList<Event>()
-        val realm = Realm.getDefaultInstance()
-        try {
-            realm.where(EventDbo::class.java)
-                    .findAll()
-                    .mapTo(result) { eventMapper.mapFromDbo(it) }
-        } catch (e: Exception) {
-            println(e)
-        } finally {
-            realm.close()
-            return Observable.just(result)
-        }
+        return Observable.create<List<EventDbo>> {
+                    val realm = Realm.getDefaultInstance()
+                    try {
+                        val all = realm.where(EventDbo::class.java)
+                                .findAll()
+                        it.onNext(all)
+                        it.onCompleted()
+                    } catch (e: Exception) {
+                        it.onError(e)
+                    }
+                    realm.close()
+                }
+                .flatMapIterable {it}
+                .map { eventMapper.mapFromDbo(it) }
+                .toList()
     }
 }
