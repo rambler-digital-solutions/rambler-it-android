0f8e5942fcc3f3c36772ebe37036c1221f946dbc
Shchenev Vadim <v.shchenev@rambler-co.ru>
2017-03-06 16:40:31 +0300

    game library added

--- app/src/main/kotlin/ru/rambler/it/presentation/RamblerITApplication.kt
+++ app/src/main/kotlin/ru/rambler/it/presentation/RamblerITApplication.kt
@@ -1,12 +1,14 @@
 package ru.rambler.it.presentation
 
 import android.app.Application
+import com.robohorse.rocketgame.RocketGame
 import ru.rambler.it.other.di.initializeAppComponent
 
 class RamblerITApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        initializeAppComponent(this);
+        initializeAppComponent(this)
+        RocketGame.initialize(this)
     }
 }
